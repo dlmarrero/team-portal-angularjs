41d72b68aae323fa3ac425fc497cd14b16d6ad56
Davy Marrero <dleefilms@gmail.com>
2017-11-06 13:14:23 -0500

    eod 06nov17

--- dist/features/accounts/login.ctrl.js
+++ dist/features/accounts/login.ctrl.js
@@ -1,13 +1,19 @@
 angular.module('app')
     .controller('loginCtrl', loginCtrl);
 
-loginCtrl.$inject = ['$scope', '$location', 'authService', '$state'];
-function loginCtrl($scope, $location, authService, $state) {
+loginCtrl.$inject = ['$scope', '$location', 'authService', '$state', '$rootScope'];
+function loginCtrl($scope, $location, authService, $state, $rootScope) {
 
     $scope.login = login;
     $scope.loginData = {};
-    $scope.message = "";
+    // $rootScope.message = "";
 
+    $scope.$on('messageUpdate',function (event, data) {
+        $rootScope.message = data;
+        console.log("Data:",data);
+        console.log("Event:",event);
+        console.log("$scope.message",$scope.message)
+    });
 
     function login() {
         authService.login($scope.loginData)
@@ -15,7 +21,9 @@ function loginCtrl($scope, $location, authService, $state) {
                 $state.transitionTo('app.main', {}, { reload: true });
             },
             function (error_description) {
-                $scope.message = error_description.data.error_description; // Fix this
+                // $scope.message = error_description.data.error_description; // Fix this
+                console.log(error_description.data.error_description);
+                $rootScope.$broadcast('messageUpdate',error_description.data.error_description);
             });
     };
 
